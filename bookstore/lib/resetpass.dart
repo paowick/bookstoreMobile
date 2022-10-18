07416b68b07977a22649b89c74168bde9a1f2326
paowick <korn2k9@gmail.com>
2022-10-18 22:23:45 +0700

    reset password page

--- bookstore/lib/resetpass.dart
+++ bookstore/lib/resetpass.dart
@@ -1,6 +1,7 @@
+import 'package:bookstore/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+
 import 'package:bookstore/main.dart';
 
 class Resetpass extends StatefulWidget {
@@ -11,12 +12,73 @@ class Resetpass extends StatefulWidget {
 }
 
 class _ResetpassState extends State<Resetpass> {
+  TextEditingController _email = TextEditingController();
+
+  final auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(appName),
       ),
+      body: Form(
+        autovalidateMode: AutovalidateMode.always,
+        child: ListView(
+          children: <Widget>[
+            emailTextFormField(),
+            sendToEmail(context),
+          ],
+        ),
+      ),
     );
+  }
+
+  TextFormField emailTextFormField() {
+    return TextFormField(
+      controller: _email,
+      validator: (value) {
+        if (!validateEmail(value!))
+          return 'Please fill in E-mail field';
+        else
+          return null;
+      },
+      keyboardType: TextInputType.emailAddress,
+      textInputAction: TextInputAction.next,
+      decoration: const InputDecoration(
+        labelText: 'E-mail',
+        icon: Icon(Icons.email),
+        hintText: 'x@x.com',
+      ),
+    );
+  }
+
+  bool validateEmail(String value) {
+    RegExp regex = RegExp(
+        r'^(([^>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+
+    return (!regex.hasMatch(value)) ? false : true;
+  }
+
+  ElevatedButton sendToEmail(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () {
+          resetPassword(_email.text);
+          Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(builder: (context) => const LoginPage()),
+              ModalRoute.withName('/'));
+        },
+        child: Text('Send Request'));
+  }
+
+  @override
+  Future<void> resetPassword(String email) async {
+    try {
+      await auth.sendPasswordResetEmail(email: email);
+    } on FirebaseAuthException catch (e) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text("Wrong Email")));
+      print(e);
+    }
   }
 }
